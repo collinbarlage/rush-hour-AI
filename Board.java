744a8e0b51a085500f19b9761e1463db53e55c92
Collin Barlage <cjb356@drexel.edu>
2018-10-03 23:11:47 -0400

    Implement loading board

--- Board.java
+++ Board.java
@@ -3,19 +3,42 @@ import java.util.Vector;
 
 public class Board implements java.io.Serializable {
     IO io = new IO();
-    // private Vector<Question> questions = new Vector<Question>();
+    private String boardArray[][] = new String[6][6];
 
     public Board() {
+        // default does nothing
+    }
+
+    public void load(String init) {
+        Vector<char> chars = new Vector<char>();
+        for (int i=0; i<init.length(); i++) {
+            if (init.charAt(i) != '|') chars.add(init.charAt(i));
+        }
+
+        if(chars.size() != 36) {
+            io.outputln("Oh geez, something went wrong. Check your argument string");
+            return;
+        }
+
+        // load  chars onto board
+        for (int j=0; j<6; j++) {
+            for (int i=0; i<6; i++) boardArray[i,j] = chars.get(j*6 + i);
+        }
 
     }
 
-    public Board(String BoardName) {
-        
+    public String get(int x, int y) {
+        return boardArray[x][y];
     }
 
     public void display() {
-        io.outputln("\n\n----------------------------------------------");
-
+        io.outputln(" ------ ");
+        for (int j=0; j<6; j++) {
+            io.output("|");
+            for (int i=0; i<6; i++) io.output(boardArray[i,j]);
+            io.output("|\n");
+        }
+        io.outputln(" ------ ");
     }
 
 
--- Main.java
+++ Main.java
@@ -1,30 +1,27 @@
 public class Main {
 
     public static void main(String[] args) {
-
         IO io = new IO();
-
-        boolean error = true;
-
-        io.outputln("\n  R U S H    H O U R\n-----------------------");
+        io.outputln("\n  R U S H     H O U R\n-----------------------");
         io.outputln(" Collin Barlage CS 380\n");
 
+        // check for errors 
         if (args.length < 1) {
             io.outputln("YIKES you didn't give me a command argument");
-            io.outputln("Try running something like  > sh run.sh command");
+            io.outputln("Try running something like  > sh run.sh command\n\n");
             return;
         }
+
+        // get args
         String command = args[0];
         String arg     = "";
         if (args.length > 1) arg = args[1];
+        io.outputln("command: "+ command + "\nArg: "+ arg + "\n");
 
-        io.outputln("command: "+ command + "\nArg: "+ arg);
-
-
-        io.outputln("");
-
-        Game newGame = new Game(); 
-        newGame.display();
+        // create game board
+        Board newBoard = new Board();  
+        newBoard.load(arg);
+        newBoard.display();
 
 
 
