9123abbe1a0a50e1ad91e88d6be9d3dd696e0e8d
Collin Barlage <cjb356@drexel.edu>
2018-10-10 18:03:07 -0400

    QUickly copy boards before adding them

--- Main.java
+++ Main.java
@@ -41,12 +41,12 @@ public class Main {
                     Board forward = new Board(newBoard);
                     while (forward.canMove(forward.cars.get(c), 1)) {
                         forward.move(forward.cars.get(c), 1);
-                        nextBoards.add(forward);
+                        nextBoards.add(new Board(forward));
                     }      
                     Board backward = new Board(newBoard);
                     while (backward.canMove(backward.cars.get(c), -1)) {
                         backward.move(backward.cars.get(c), -1);
-                        nextBoards.add(backward);
+                        nextBoards.add(new Board(backward));
                     }       
                 }
                 newBoard.printBoards(nextBoards);
