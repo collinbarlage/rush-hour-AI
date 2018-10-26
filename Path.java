fb9db2c5e12e5b96c4c662ca6ba62dced92415b4
Collin Barlage <cjb356@drexel.edu>
2018-10-26 17:26:50 -0400

    Easy prune

--- Path.java
+++ Path.java
@@ -154,7 +154,7 @@ public class Path implements java.io.Serializable {
         Path reducedPath = new Path();
         reducedPath.parent = og.parent;
         for(int i=0; i<og.size(); i++) {
-            if(og.get(i).xIndex >= xIndex) {
+            if(og.get(i).xIndex > 0) {
                 reducedPath.add(og.get(i));
             }
         }
