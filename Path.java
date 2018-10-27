44698166bc33d3aa2d48ac7b22ebb117cebb4129
Collin Barlage <cjb356@drexel.edu>
2018-10-27 01:07:53 -0400

    More pruning

--- Path.java
+++ Path.java
@@ -155,7 +155,9 @@ public class Path implements java.io.Serializable {
         Path reducedPath = new Path();
         reducedPath.parent = og.parent;
         for(int i=0; i<og.size(); i++) {
-            if(og.get(i).xIndex > 0 || xIndex == 0) {
+            if(og.get(i).xIndex == 0 && xIndex > 3 ) {
+                //do nothing lol
+            } else if(og.get(i).xIndex > 0 || xIndex == 0 ) {
                 reducedPath.add(og.get(i));
             }
         }
